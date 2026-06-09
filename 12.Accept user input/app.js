86729624b27cd6cf7e9f52b279d3429fbeadf983
Devang-820 <devangs820@gmail.com>
2026-06-09 11:15:06 +0530

    Add files via upload

---
+++ 12.Accept user input/app.js
@@ -0,0 +1,11 @@
+//let name;
+//name = window.prompt("what is your name")
+
+//console.log(name);
+
+let username;
+document.getElementById("mysubmit").onclick = function(){
+username = document.getElementById("mytext").value
+console.log(username)
+}
+
